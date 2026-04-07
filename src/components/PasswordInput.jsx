3f2e28e0efffb9845c3d49e8a083e50a455a1047
abbi portwood <apwood09@gmail.com>
2026-04-07 11:10:26 -0400

    test ran and passed

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -1,9 +1,22 @@
 // Code PasswordInput Component Here
+import React, { useState } from 'react'; 
 
 function PasswordInput (){
+    const [password, setPassword] = useState(''); 
+
+    const handleChange = (event) => {
+        console.log("Entering password...");
+        setPassword(event.target.value); 
+    }; 
+
     return(
-        <></>
-    )
+        <input 
+            type='password'
+            value={password}
+            onChange={handleChange}
+            placeholder='Enter Your Passowrd'
+        />
+    ); 
 }
 
 export default PasswordInput;
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,9 +1,24 @@
 // Code SubmitButton Component Here
+import React from 'react';
 
 function SubmitButton (){
+
+    const handleEnter = () => {
+        console.log("Mouse Entering"); 
+    }; 
+
+    const handleLeave = () => {
+        console.log("Mouse Exiting"); 
+    }; 
+
     return(
-        <></>
-    )
+         <button 
+            onMouseEnter={handleEnter} 
+            onMouseLeave={handleLeave}
+        >
+            Submit Password
+        </button>
+    ); 
 }
 
 export default SubmitButton;
